fbbf3efda18c9875add781b460b2b9c4837101b5
Giuli Ilustre <ilustre.giuli@gmail.com>
2021-11-01 21:30:25 -0300

    Atribuicoes e Unarios

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -4,18 +4,7 @@ public class ParOuImpar {
 
 	public static void main(String[] args) {
 		
-		int parOuImpar = 7;
-		boolean par = false;
 		
-		par = ( (parOuImpar % 2) == 0 ? true : false);
-		
-		System.out.println(par);
-		
-		boolean impar = false;
-		
-		impar = ( (parOuImpar % 2) > 0 ? true : false);
-		
-		System.out.println(impar);
 		
 
 	}
