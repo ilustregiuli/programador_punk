562acecd086bb941f7fb1b4a5f8fca6ae3c07eee
Giuli Ilustre <ilustre.giuli@gmail.com>
2021-11-01 22:52:56 -0300

    Melhoria no código

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -3,19 +3,23 @@ package videos_curtos;
 public class ParOuImpar {
 
 	public static void main(String[] args) {
-		
+
 		int parOuImpar = 7;
 		boolean par = false;
 
-		par = ( (parOuImpar % 2) == 0 ? true : false);
+		par = (parOuImpar % 2) == 0;
 
 		System.out.println(par);
 
 		boolean impar = false;
 
-		impar = ( (parOuImpar % 2) > 0 ? true : false);
+		impar = (parOuImpar % 2) > 0 ;
 
 		System.out.println(impar);
+		
+		
+		
+		
 		
 
 	}
